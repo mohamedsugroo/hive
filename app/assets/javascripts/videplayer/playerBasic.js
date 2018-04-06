e7c7af3fc061e03473603424440bd053d56493fc
mohamedsugroo <mohamedsugroo@gmail.com>
2018-04-06 05:16:24 +0100

    videoplayer styles

---
+++ app/assets/javascripts/videplayer/playerBasic.js
@@ -0,0 +1,47 @@
+// video player pasic funtions 
+
+
+// variable Declaration
+var vid;
+// vars in use
+vid = document.querySelectorAll("video");
+
+// play pause functions
+
+function playPause() {
+
+    if ( vid.play = true ) {
+        consol.log("Video playing");
+    } else {
+        console.log("video paused");
+    }
+
+}
+
+
+// video loop functions
+
+function loop(){
+
+}
+
+// video volume contol functions
+
+function mutebtn() {
+
+}
+
+
+// video Time update functions
+
+function timeUpdate() {
+
+}
+
+
+// setting functions
+
+
+function settings() {
+
+}
