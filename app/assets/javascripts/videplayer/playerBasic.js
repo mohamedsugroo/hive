a19e9abba69627a0a7d7f477800149190bd63ac4
mohamedsugroo <mohamedsugroo@gmail.com>
2018-04-08 14:43:47 +0100

    video player

--- app/assets/javascripts/videplayer/playerBasic.js
+++ app/assets/javascripts/videplayer/playerBasic.js
@@ -2,17 +2,24 @@ $(document).ready(function () {
 
     // video player pasic funtions 
     // variable Declaration
-    var vid,btnplayPause,loopBtn,muteBtn,curSpan,durSpan;
+    var vid,btnplayPause,loopBtn,muteBtn,curSpan,durSpan,muteBtn, fullscreenBtn;
     // vars in use
     vid = document.querySelector("#videoPlayer");        
     btnplayPause = document.querySelector(".playPause");
+    muteBtn = document.querySelector(".muteBtn");        
     loopbtn = document.querySelector(".loopBtn");    
     curSpan = document.querySelector(".current-time");
     durSpan = document.querySelector(".duration");
+    fullscreenBtn = document.getElementById("btnFullscreen");
+    
 
     // var with functions
     btnplayPause.addEventListener('click', playPause, false);
-    loopbtn.addEventListener('click', vidLoop, false);    
+    vid.addEventListener('click', playPause, false);    
+    loopbtn.addEventListener('click', vidLoop, false);
+    muteBtn.addEventListener('click', mutebtn, false);
+    fullscreenBtn.addEventListener('click', fullscreenEnter, false);
+    
 
     // play pause functions
     function playPause() {
@@ -32,6 +39,17 @@ $(document).ready(function () {
     // video volume contol functions
     function mutebtn() {
 
+        if (vid.muted) {
+            vid.muted = false;
+            $('.muteBtn').addClass("mutedIcon");
+            $('.muteBtn').removeClass("muteIcon");
+        }else {
+            vid.muted = true;
+            $('.muteBtn').addClass("muteIcon");
+            $('.muteBtn').removeClass("mutedIcon");
+            
+        }
+
     }
 
     // video Time update functions
@@ -46,11 +64,9 @@ $(document).ready(function () {
         if(durmins < 10){ durmins = "0"+durmins; }
         if(dursecs < 10){ dursecs = "0"+dursecs; }
 
-
-        console.log(nt);
-
         $( "#current-time" ).html(curmins+":"+cursecs);
         $( "#duration" ).html(durmins+":"+dursecs);
+
         
     }
 
@@ -59,6 +75,83 @@ $(document).ready(function () {
     function settings() {
 
     }
+
+    // fullscreen master
+
+    function fullscreenToggle() {
+        console.log("fullscreen")
+    }
+
+    function fullscreenEnter() {
+        if (vid.requestFullScreen) {
+            vid.requestFullScreen();
+          }else if (vid.webkitRequestFullScreen) {
+            vid.webkitRequestFullScreen();
+          }else if(vid.mozRequestFullScreen){
+            vid.mozRequestFullScreen();
+          }
+
+          $(".uhPlayer").addClass("fullscreenMode");
+    }
+
+
+
+
+
+    var fullscreen = false;
+    if(document.fullscreenEnabled)
+        fullscreen = {
+            request: "requestFullscreen",
+            element: "fullscreenElement",
+            exit: "exitFullscreen",
+            event: "fullscreenchange"
+        };
+    else if(document.msfullscreenEnabled)
+        fullscreen = {
+            request: "msRequestFullscreen",
+            element: "msFullscreenElement",
+            exit: "msExitFullscreen",
+            event: "msfullscreenchange"
+        };
+    else if(document.mozfullscreenEnabled)
+        fullscreen = {
+            request: "mozRequestFullScreen",
+            element: "mozFullScreenElement",
+            exit: "mozCancelFullScreen",
+            event: "mozfullscreenchange"
+        };
+    else if(document.webkitFullscreenEnabled)
+    fullscreen = {
+        request: "webkitRequestFullscreen",
+        element: "webkitFullscreenElement",
+        exit: "webkitExitFullscreen",
+        event: "webkitfullscreenchange"
+    };
+
+
+    if(fullscreen){
+        document.addEventListener(fullscreen["event"], function(event){
+            if(document[fullscreen["element"]]){
+                $(".uhPlayer").addClass("fullscreenMode");                
+
+            } else {
+                $(".uhPlayer").removeClass("fullscreenMode");                
+
+            }
+        console.log("bariiiiiiiis");                
+        }, false);
+    }
+
+
+
+
+    
+
+
+
+
+
+    // end fullScreenToggle
 
 
 });
